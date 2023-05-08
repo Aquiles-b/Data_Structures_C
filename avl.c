eaa5b1c9c5d80a27d5dc7b93fa997116f8493739
Aquiles-bb <brunoaquiles@ufpr.br>
2023-05-08 16:03:26 -0300

    InsereNodo feito

--- avl.c
+++ avl.c
@@ -11,17 +11,17 @@ struct aluno* getAluno1(){
     if(!retorno)
         exit(1);
     retorno->nome = malloc(sizeof("Bruno Aquiles de Lima"));//sizeof conta o \0
-	if(!retorno->nome)
-		exit(1);
+    if(!retorno->nome)
+        exit(1);
     retorno->nomeDinf = malloc(sizeof("bal22"));
     if(!retorno->nomeDinf)
         exit(1);
-    
-	strcpy(retorno->nome, "Bruno Aquiles de Lima");
+
+    strcpy(retorno->nome, "Bruno Aquiles de Lima");
     strcpy(retorno->nomeDinf, "bal22");
     retorno->grr = 20221230;
 
-	return retorno;
+    return retorno;
 }
 
 struct aluno* getAluno2(){
@@ -30,7 +30,7 @@ struct aluno* getAluno2(){
 
 void imprimirDadosAlunos(){
     struct aluno* aluno = getAluno1();
-    
+
     printf("Trabalho de %s\n", aluno->nome);
     printf("Login Dinf %s\n", aluno->nomeDinf);
     printf("GRR %u\n\n", aluno->grr);
@@ -42,7 +42,7 @@ void imprimirDadosAlunos(){
     aluno = getAluno2();
     if(!aluno)
         return;
-    
+
     printf("...E... \n\n");
     printf("Trabalho de %s\n", aluno->nome);
     printf("Login Dinf %s\n", aluno->nomeDinf);
@@ -53,38 +53,73 @@ void imprimirDadosAlunos(){
     free(aluno);
 }
 
-/* Cria um nodo. Retorna NULL em caso de problemas.*/
-struct nodo *criaNodo()
+struct nodo *criaNodo(int chave)
 {
-    struct nodo *aux = malloc(sizeof(struct nodo));
-    if (aux == NULL)
+    struct nodo *n = malloc(sizeof(struct nodo));
+    if (n == NULL)
         return NULL;
 
-    return aux;
+    n->chave = chave;
+    n->fe = NULL;
+    n->fd = NULL;
+    n->pai = NULL;
+
+    return n;
+}
+
+struct nodo *insereNodo(struct nodo *n, int chave)
+{
+    if (n->chave == chave)
+        return NULL;
+
+    if (n->chave > chave) {
+        if (n->fe != NULL)
+            return insereNodo(n->fe, chave);
+
+        n->fe = criaNodo(chave);
+        n->fe->pai = n;
+        return n->fe;
+    }
+    if (n->fd != NULL)
+        return insereNodo(n->fd, chave);
+
+    n->fd = criaNodo(chave);
+    n->fd->pai = n;
+    return n->fd;
 }
 
 struct nodo *inserir(struct nodo **raiz, int chave)
 {
+    struct nodo *n = insereNodo(*raiz, chave);
+    if (n == NULL)
+        return NULL;
 
-	return NULL;
+    return n;
 }
 
 int excluir(struct nodo **raiz, int chave)
 {
-	return -99;
+    return -99;
 }
 
 struct nodo *buscar(struct nodo *nodo, int chave)
 {
-	return NULL;
+    if (nodo == NULL)
+        return NULL;
+    if (nodo->chave == chave)
+        return nodo;
+    if (nodo->chave < chave)
+        return buscar(nodo->fd, chave);
+
+    return buscar(nodo->fe, chave);
 }
 
 void imprimirEmOrdem(struct nodo *raiz)
 {
-	printf("Imprimindo em ordem\n");
+    printf("Imprimindo em ordem\n");
 }
 
 void imprimirEmLargura(struct nodo *raiz)
 {
-	printf("Imprimindo em largura\n");
+    printf("Imprimindo em largura\n");
 }
